026aae38d5c89c682d1fb774f9b0143e4f1952b7
Siddharth Poduval <sid12598@Siddharths-MacBook-Pro.local>
2017-10-16 16:32:54 +0530

    Revised version

--- news.sh
+++ news.sh
@@ -1,6 +1,3 @@
-## API Key
-apikey="b09c8472e6c744638ebc63e02650beea"
-
 ## Check for missing options
 ## Checks input command for any options
 ## Presence of illegal options is ignored at this stage
@@ -11,6 +8,12 @@ then
 	exit 0
 fi;
 
+## Set defaults
+## API Key
+apiKey=b09c8472e6c744638ebc63e02650beea
+## Language
+language=en
+
 ## Test Internet Connection
 ## Accepts argument and writes status of internet connection on it
 ## true - connected; false - not connected 
@@ -19,81 +22,11 @@ testForInternetConnection() {
 	then
 		eval "$1='true'"
 	else
-		
+
 		eval "$1='false'"
 	fi;
 }
 
-## Check for preferences 
-## Accepts an arguemnt and writes status of preferences file on it
-## true - file exists; false - file doesn't exist
-checkPrefFileExists() {
-	if [ -n "$(find . -name pref.md)" ];
-	then
-		eval "$1='true'"
-	else
-		eval "$1='false'"
-	fi;
-}
-	# echo "Preferences file not found"
-
-## Check Category Exists
-checkCategory() {
-	case "$1" in
-		"business"|"entertainment"|"gaming"|"general"|"music"|"politics"|"science-and-nature"|"sport"|"technology")
-			eval "$2='true'"
-			;;
-		*)
-			eval "$2='false'"
-			;;
-	esac
-}
-
-## Get Sources from JSON Response
-getSources() {
-	curl -sS -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/sources" | jq '[.sources | {id: .[].id}]' > sources.json
-	correctedSources=$(cat sources.json | jq '.[].id')
-	echo "$correctedSources"
-}
-
-## Show articles
-showArticles() {
-	case $OPTION in
-		s)
-			curl -s -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/articles?apiKey=$apikey&source=$2" | jq '. | {title: .articles[].title}' > articles/title.json
-			curl -s -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/articles?apiKey=$apikey&source=$2" | jq '. | {description: .articles[].description}' > articles/description.json
-			opTitle=$(cat articles/title.json | jq '.title')
-			opDescription=$(cat articles/description.json | jq '.description')
-			echo "$opTitle" > articles/title.json
-			echo "$opDescription" > articles/description.json
-			paste -d '\n' articles/title.json articles/description.json > articles/final.json
-			echo ""
-			awk -v n=2 '1; NR % n == 0 {print ""}' articles/final.json
-			;;
-		c)
-			## Run loop for length of array in the entered categories .json file
-			for key in $(cat pref/$2.json | jq '.[]'); do
-				source="${key%\"}"
-				source="${source#\"}"
-				linkGot="https://newsapi.org/v1/articles?apiKey=$apikey&source=${source}"
-			    curl -s -X GET -H "Content-Type: application/json" $linkGot | jq '. | {title: .articles[].title}' > articles/title.json
-				curl -s -X GET -H "Content-Type: application/json" $linkGot | jq '. | {description: .articles[].description}' > articles/description.json
-				opTitle=$(cat articles/title.json | jq '.title')
-				opDescription=$(cat articles/description.json | jq '.description')
-				echo "$opTitle" > articles/title.json
-				echo "$opDescription" > articles/description.json
-				paste -d '\n' articles/title.json articles/description.json > articles/final.json
-				echo ""
-				awk -v n=2 '1; NR % n == 0 {print ""}' articles/final.json
-			done
-			;;
-
-		*)
-			echo "News fetched successfully"
-	esac
-}
-
-## Evaluate request
 testForInternetConnection conn
 if [ $conn = "false" ];
 then
@@ -104,30 +37,84 @@ then
 	exit 0
 fi;
 
-checkPrefFileExists pref
+## Check for preferences 
+## Accepts an arguemnt and writes status of preferences file on it
+## true - file exists; false - file doesn't exist
+checkPrefFileExists() {
+	if [ -n "$(find . -name pref.json)" ];
+	then
+		eval "$1='true'"
+	else
+		eval "$1='false'"
+	fi;
+}
 
-while getopts "ahps" OPTION; do
+checkPrefFileExists pref
+if [ $pref = "false" ];
+then
+	echo "Preferences files created."
+	printf '{ }' | cat > pref.json
+fi;
+
+
+viewSources() {
+	echo "For $1, the available sources in $language are: "
+	curl -sS -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/sources?category=$1&language=$language" | jq '.sources[].id' #> sources.json
+	#correctedSources=$(cat sources.json | jq '.[].id')
+	#echo "$correctedSources"
+}
+
+acceptValidCategory() {
+	echo "Categories: business, entertainment, gaming, general, music, politics, science-and-nature, sport, technology."
+	read -p "Enter category: " category
+	case $category in
+		business|entertainment|gaming|general|music|politics|science-and-nature|sport|technology)
+			;;
+		*)
+			echo "Invalid category. Try again"
+			acceptValidCategory $1
+			;;
+	esac
+}
+
+modifyPreferences() {
+	echo "Preferences mode"
+	acceptValidCategory $category
+	viewSources $category
+	read -p "Enter your preferred source ID: " source_id
+	echo "{\"$category\":\"$source_id\"}" | cat > temp.json
+	temp=$(jq -s add pref.json temp.json)
+	echo $temp | cat > pref.json
+	echo "Preference for $category has been set to $source_id"
+	#jq --arg category $category --arg id $source_id | '.category=id' pref.json
+}
+
+viewArticle() {
+	echo "Article mode"
+	acceptValidCategory $category
+	source_id=$(jq --arg category $category ".$category" pref.json)
+	if [ $source_id = null ];
+	then
+		echo "Source preference not found"
+		viewSources $category
+		read -p "Enter required source: " source_id
+		source_id=$(echo "\"$source_id\"")
+	fi;	
+	echo "Available sortBy orders are: "
+	curl -s -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/sources?category=$category&language=$language" | jq --arg source_id "$source_id" ".sources[]|select(.id==$source_id)|.sortBysAvailable"
+	read -p "Enter sort by: " sortBy
+	source_id=$(sed 's/"//g' <<< $source_id) 
+	curl -s -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/articles?apiKey=$apiKey&source=$source_id&sortBy=$sortBy" | jq '.articles[]|.title,.author,.description'
+}
+
+while getopts "ahp" OPTION; do
 	case $OPTION in
 		a)
-			echo "Articles options selected"
-			while getopts "soc" OPTION; do
-				case $OPTION in
-					c) 
-						read -p "Enter the category: " category
-						args="c"
-						showArticles $args $category
-						;;
-
-					s)
-						read -p "Enter the source name: " source
-						echo "Selected source is: $source"
-						args="s"
-						showArticles $args $source
-						;;
-
-				esac
-			done
-			showArticles
+			viewArticle
+			exit 0
+			;;
+		p)
+			modifyPreferences
 			exit 0
 			;;
 		h)
@@ -140,9 +127,6 @@ while getopts "ahps" OPTION; do
 			echo "Your one stop shop for the latest news, right from your terminal"
 			echo ""
 			echo "To view articles, run $0 -a"
-			echo "Options: -s = specify source; -o = specify sort-by order; -c = specify category"
-			echo ""
-			echo "To view sources, run $0 -s"
 			echo ""
 			echo "To modify specific preferences, run $0 -p"
 			echo ""
@@ -150,32 +134,7 @@ while getopts "ahps" OPTION; do
 			echo "Do not combine primary options!"
 			exit 0
 			;;
-		s)
-			echo "Following sources are available:"
-			getSources
-			;;
-		p)
-			checkPrefFileExists pref
-			if [ $pref = "false" ];
-			then
-				echo "Preferences files created."
-				printf '{\n}' | cat > pref.md
-			fi;
-			status="false"
-			while [ $status = "false" ];
-			do
-				echo "Categories: business, entertainment, gaming, general, music, politics, science-and-nature, sport, technology."
-				read -p "Enter the category you would like to modify: " category
-				checkCategory category status
-				if [ $status = "false" ]
-				then
-					echo "This is not a valid category"
-				fi;
-			done
-			curl -sS -X GET -H "Content-Type: application/json" "https://newsapi.org/v1/sources?category=$category"
-			read -p "Enter the preference for $category: " preference
-			echo "Preference entered"
-			exit 0
-			;;
 	esac
 done
+
+
